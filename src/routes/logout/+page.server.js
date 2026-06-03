b0cf98fd27723e27549b8feea3e15127fda0a376
SarZag21 <SarZag21@htl-shkoder.com>
2026-06-03 10:32:40 +0200

    Add logout functionality

---
+++ src/routes/logout/+page.server.js
@@ -0,0 +1,15 @@
+import { invalidateSession } from '$lib/server/auth.js';
+import { redirect } from '@sveltejs/kit';
+
+export const actions = {
+    logout: async ({ cookies }) => {
+        const sessionId = cookies.get('session');
+        if (sessionId) {
+            await invalidateSession(sessionId);
+            cookies.delete('session', {
+                path: '/'
+            });
+        }
+        throw redirect(303, '/');
+    }
+};
