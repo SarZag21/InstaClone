6a1f19ae1de95b56f2552a2175d8e292f4728603
SarZag21 <SarZag21@htl-shkoder.com>
2026-06-17 11:00:36 +0200

    Add comments for logout file

--- src/routes/logout/+page.server.js
+++ src/routes/logout/+page.server.js
@@ -3,13 +3,18 @@ import { redirect } from '@sveltejs/kit';
 
 export const actions = {
     logout: async ({ cookies }) => {
+
+     // Get current session cookie
         const sessionId = cookies.get('session');
         if (sessionId) {
+         // Remove session from database
             await invalidateSession(sessionId);
+          // Delete session cookie from browser
             cookies.delete('session', {
                 path: '/'
             });
         }
+     // Redirect user to homepage after logout
         throw redirect(303, '/');
     }
 };
